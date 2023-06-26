9fc87f3c6642a058d91a35bdf169bbd5bea80a75
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-06-26 06:45:06 +0200

    Setting

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField("Name", max_length=50)
+    vorname = models.CharField("Vorname", max_length=50)
+    eintritt = models.DateField("Eintritt", auto_now=False, auto_now_add=True)
+    update = models.DateField("Update", auto_now=True, auto_now_add=False)
+    comment = models.TextField("Kommentar", blank=True, null=True)
+    wertung = models.IntegerField("Wertung")
+    class Meta:
+        verbose_name = "Person"
+        verbose_name_plural = "Personen"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Person_detail", kwargs={"pk": self.pk})
+
